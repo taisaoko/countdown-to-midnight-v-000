5221f485258d4617f9b1ba80178e94d28483f2d3
taisaoko <ttd3calvin@gmail.com>
2018-08-16 15:23:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
 
-def countdown (number)
+def countdown(number)
   number = 10
   until number == 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   "HAPPY NEW YEAR!"
-  
 end
+
+def countdown_
